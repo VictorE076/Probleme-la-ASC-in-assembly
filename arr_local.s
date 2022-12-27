7b31a63f84203789765c5e3a27b087713347ac3d
VictorE076 <79158926+VictorE076@users.noreply.github.com>
2022-12-27 03:52:52 +0200

    Add files via upload

---
+++ arr_local.s
@@ -0,0 +1,37 @@
+//Construim un array mic in procedura si calculam numarul de aparitii a unui numar dat in array-ul
+//respectiv (+ afisare);
+.data
+	n: .long 9
+	
+	fpr: .asciz "%d "
+	fend: .asciz "\n"
+.text
+Counting:
+	pushl %ebp
+	movl %esp, %ebp
+	
+		pushl %ebx
+		pushl %esi
+		pushl %edi
+		
+		
+	///
+		
+		
+		popl %edi
+		popl %esi
+		popl %ebx
+	
+	popl %ebp
+	ret
+
+.globl main
+main:
+etexit:
+	movl $1, %eax
+	xor %ebx, %ebx
+	int $0x80
+	
+	
+	
+// v: .long 12, 56, 34, 12, 78, 12, 12, 34, 56
