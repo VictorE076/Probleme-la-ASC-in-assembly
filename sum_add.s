7c592cd35bbc79491854423c3823a66cc2c535d0
VictorE076 <79158926+VictorE076@users.noreply.github.com>
2023-01-12 01:44:49 +0200

    Add files via upload

---
+++ sum_add.s
@@ -0,0 +1,33 @@
+// add(x, y, &s);
+.data
+x: .long 2
+y: .long 3
+s: .space 4
+
+.text
+
+add:
+
+movl 4(%esp), %eax
+addl 8(%esp), %eax
+
+movl 12(%esp),%ebx
+movl %eax, 0(%ebx)
+
+ret
+
+.global main
+main:
+
+pushl $s
+pushl y
+pushl x
+call add
+popl %edx
+popl %edx
+popl %edx
+
+ex:
+mov $1, %eax
+xor %ebx, %ebx
+int $0x80
